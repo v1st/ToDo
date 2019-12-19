9f237641fa2313f8618d0c3ee23373c9ea21e739
v1st <brandon_m918@yahoo.com>
2019-12-19 08:47:16 -0500

    setting up react

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Home from './pages/Index';
+import Nav from './components/organisms/NavBar';
+import Footer from './components/organisms/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />      
+        <Home />      
+      <Footer />      
     </div>
   );
 }
---
+++ client/src/pages/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Index() {
+  return (
+    <main class="container py-5">
+      <div class="row justify-content-center">
+        <div class="col-sm">
+          <h1><strong>Manage your <br /> projects with Task.io</strong></h1>
+          <p class="lead">Schedule out your daily tasks</p>
+
+          <a class="btn btn-primary px-4 py-2" href="/signup" role="button">Get Started</a>
+        </div>
+      </div>
+    </main>
+  )
+}
